b07cce3a5fa8c65531a74f8bb019567c64cc128e
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-20 18:00:56 +0400

    XPO

---
+++ CS/XPO/NonPersistentFiltering/NonPersistentFiltering.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using NonPersistentFiltering.Blazor.Server.Services;
+
+namespace NonPersistentFiltering.Blazor.Server;
+
+public class NonPersistentFilteringBlazorApplication : BlazorApplication {
+    public NonPersistentFilteringBlazorApplication() {
+        ApplicationName = "NonPersistentFiltering";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += NonPersistentFilteringBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void NonPersistentFilteringBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ CS/XPO/NonPersistentFiltering/NonPersistentFiltering.Module/Module.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Model.Core;
+using DevExpress.ExpressApp.Model.DomainLogics;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using NonPersistentObjectsDemo.Module.BusinessObjects;
+
+namespace NonPersistentFiltering.Module;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.ModuleBase.
+public sealed class NonPersistentFilteringModule : ModuleBase {
+    public NonPersistentFilteringModule() {
+		// 
+		// NonPersistentFilteringModule
+		// 
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
+    }
+    public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
+        ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
+        return new ModuleUpdater[] { updater };
+    }
+    public override void Setup(XafApplication application) {
+        base.Setup(application);
+        // Manage various aspects of the application UI and behavior at the module level.
+        application.SetupComplete += Application_SetupComplete;
+    }
+    private void Application_SetupComplete(object sender, EventArgs e) {
+        Application.ObjectSpaceCreated += Application_ObjectSpaceCreated;
+    }
+    private void Application_ObjectSpaceCreated(object sender, ObjectSpaceCreatedEventArgs e) {
+        var npos = e.ObjectSpace as NonPersistentObjectSpace;
+        if (npos != null) {
+            new ArticleAdapter(npos);
+            new ContactAdapter(npos);
+        }
+    }
+    public override void CustomizeTypesInfo(ITypesInfo typesInfo) {
+        base.CustomizeTypesInfo(typesInfo);
+        CalculatedPersistentAliasHelper.CustomizeTypesInfo(typesInfo);
+    }
+}
